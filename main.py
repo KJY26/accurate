8b406d3f90f8b56c70ea95769c1383c50c0e0d06
김정윤 <jeongykim16@gmail.com>
2022-05-19 22:58:10 +0900

    Initial Commit

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ import xml.etree.ElementTree as et
 def main():
     local = time.localtime(time.time())
 
+    legend = ['강수형태','습도','1시간 강수량','기온','동서바람성분','풍향','남북바람성분','풍속']
+
     url = 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst'
-    params ={'serviceKey' : 'loGEsi8RLLPDoscN/+N/FQs4Gck/La0uzw51zooTczK3aWcmK6mvoHJ8tfF1uCtnKl2OdfmXYnz1QtEauhwWPA==', 'pageNo' : '1', 'numOfRows' : '1000', 'dataType' : 'XML', 'base_date' : str(local.tm_year).zfill(2)+str(local.tm_mon).zfill(2)+str(local.tm_mday).zfill(2), 'base_time' : str(local.tm_hour).zfill(2)+str(local.tm_min).zfill(2), 'nx' : '61', 'ny' : '126' }
+    params = {'serviceKey': 'loGEsi8RLLPDoscN/+N/FQs4Gck/La0uzw51zooTczK3aWcmK6mvoHJ8tfF1uCtnKl2OdfmXYnz1QtEauhwWPA==','pageNo': '1', 'numOfRows': '1000', 'dataType': 'XML','base_date': str(local.tm_year).zfill(2) + str(local.tm_mon).zfill(2) + str(local.tm_mday).zfill(2),'base_time': str(local.tm_hour).zfill(2) + str(local.tm_min).zfill(2), 'nx': '61', 'ny': '126'}
 
     response = requests.get(url, params=params)
     text = response.text
@@ -18,18 +20,26 @@ def main():
     file = et.parse("report"+".xml")
     root = file.getroot()
 
-    data = root[1][1]
+    try:
+        data = root[1][1]
+    except:
+        print("Error in data")
+        exit()
 
     result = open("results/"+fname+".txt", 'w', encoding='UTF-8')
 
-    print()
+    i = 0
     for child in data:
+        result.write(legend[i]+'\n')
+        print(legend[i])
         for value in child:
             print(value.tag, value.text)
-            result.write(value.tag+' '+value.text+"\n")
-        print()
+            result.write(value.tag+' '+value.text+' '+"\n")
         result.write("\n")
+        print()
+        i=i+1
 
+    result.close()
 
 if __name__ == '__main__':
     main()
